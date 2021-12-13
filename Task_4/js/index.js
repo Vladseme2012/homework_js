12fcd58a8504f1ecb70aec391b1359fcc7a47b8a
Vladseme2012 <vlad.seme2012@yandex.ru>
2021-12-13 20:17:11 +0300

    added parameters in html, css and js

--- Task_4/js/index.js
+++ Task_4/js/index.js
@@ -1,11 +1,13 @@
-const form = document.querySelector(".wrapper-user-information__personal-date"),
-  label = form.querySelectorAll("label"),
-  input = form.querySelectorAll("input").values,
-  btnSubmit = form.querySelector(".wrapper-user-information__button"),
-  arrLabel = label.forEach(item => console.log(item)),
-  arrInput = input.forEach(item => console.log(item)),
-  arrNode = [];
-  console.log(arrNode);
+const form = document.querySelector(".wrapper-user-information__personal-date");
+const label = form.querySelector(".label");
+const input = form.querySelector(".input");
+const btnSubmit = form.querySelector(".wrapper-user-information__button");
+const descriptionUser = document.querySelector('.wrapper-user-information__description-user');
+const modalWindow = document.querySelector('.wrapper-user-information__modal-window');
+const modalForm = modalWindow.querySelector(".wrapper-user-information__modal-form");
+const modalInput = modalWindow.querySelector('#modal-input');
+const modalButton = modalWindow.querySelector(".wrapper-user-information__modal-button");
+const arrNode = [];
 
 console.log(form);
 console.log(label);
@@ -13,5 +15,25 @@ console.log(input);
 console.log(btnSubmit);
 
 arrNode.push(form, label, input, btnSubmit);
-
 console.log(arrNode);
+
+
+btnSubmit.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log(input.value);
+  descriptionUser.innerHTML += `<p>USERNAME: ${input.value}</p>`;
+  modalWindow.style.display = 'block';
+  modalInput.value = input.value;
+  form.reset();
+});
+
+modalButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  descriptionUser.innerHTML = '';
+  modalWindow.style.display = 'none';
+});
+
+
+
+
+
