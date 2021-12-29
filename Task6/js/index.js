9b2531b36bafc7cb78a979a3a401170f75f987f9
Vladseme2012 <vlad.seme2012@yandex.ru>
2021-12-29 19:10:07 +0300

    added changes to the task

--- Task6/js/index.js
+++ Task6/js/index.js
@@ -15,43 +15,59 @@ const checkBox = table.querySelector('#check-box');
 const thName = table.querySelector('#th-name');
 const thYearOfBirth = table.querySelector('#th-yearOfBirth');
 const btnSortYearTop = table.querySelector('#btn-sort-year-top');
-const btnSortYearBottom = table.querySelector('#btn-sort-year-bottom');
+const btnSortYearBootom = table.querySelector('#btn-sort-year-bottom');
 const thDateOfEmployment = table.querySelector('#th-dateOfEmployment');
-const btnSortDateTop = table.querySelector('#btn-sort-date-top');
+const btnSortDateTop = table.querySelector('#btn-sort-year-top');
 const btnSortDateBottom = table.querySelector('#btn-sort-date-bottom');
 const thSum = table.querySelector('#th-sum');
 
 const btnDismiss = document.querySelector('#btn-dismiss');
 const btnSum = document.querySelector('#btn-sum');
+const selectedEmployeesCollection = []; // Массив выбранных сотрудников
+let sumSalary = 0;
 
+/* const employeesCollection = [
+{
+id: 0,  id нужен будет для идентификации сотрудника.  
+name:'Ivan',
+birthYear: 1978,
+hiringDate: "15.04.2010",
+salary: 1200,
+},
+]; */
 
 let employeesCollection = [];
 const dataFromLocalStorage = localStorage.getItem('employeesCollection');  //коллекция всех сотрудников
-  
+
 if (dataFromLocalStorage) {
-  employeesCollection = JSON.parse(dataFromLocalStorage); // Преобразуем строку в массив
+  employeesCollection = JSON.parse(dataFromLocalStorage);  // Преобразуем строку в массив
   drawUI(); // И отрисовываем этот массив
 }
 
-  
+function saveData() {
+	localStorage.setItem('employeesCollection', JSON.stringify(employeesCollection)); 
+}
+
+//console.log("employeesCollection - ", employeesCollection);
+
 function drawUI() {  //Фукнкция, которая обновляет данные в таблице
   tableTbody.innerHTML = '';
   employeesCollection.forEach((item, index) => {
-  const newRow = tableTbody.insertRow(tableTbody.rows.length);    
+  	const newRow = tableTbody.insertRow(tableTbody.rows.length);    
     newRow.innerHTML = `<tr>
-    <td><input type='checkbox' data-id="${item.id}"></td>
+    <td><input type="checkbox" class="select-employee-checkbox" data-id="${item.id}"></td>
     <td>${item.name}</td>
     <td>${item.birthYear}</td>
     <td>${item.hiringDate}</td>
     <td>${item.salary}</td>
   </tr>`;
-  });
+  })
   
    subtitleStaff.innerHTML = `Количество сотрудников: ${employeesCollection.length}`;
+   subtitleSum.innerHTML = `Сумма $: ${sumSalary}`; 
 }
-  
 
-addEmloyeeBtn.addEventListener('click', (event) => {
+addEmloyeeBtn.addEventListener('click', (event) => { // Добавление сотрудника
   const newEmployee = {
     id: employeesCollection.length,
     name: inputName.value,
@@ -62,30 +78,78 @@ addEmloyeeBtn.addEventListener('click', (event) => {
   
   employeesCollection.push(newEmployee);
  
-	localStorage.setItem('employeesCollection', JSON.stringify(employeesCollection));  
+	saveData();
+  inputName.value = '';
+  inputYear.value = '';
+  inputDate.value = '';
+  inputSalary.value = '';
+
   drawUI();
 });
   
-  
-  
-  
- let selectedEmploees = [];  // Массив с ИДЕНТИФИКАТОРАМИ выбранных сотрудников
-  
-// checkBox.addEventListener('change', (event) => {
-// 	console.log(event.target.dataset.id);
-//   // selectedEmploees.push(dataset.id);
-// });
-  
+table.addEventListener('click', (event) => {
+  if (event.target.className == "select-employee-checkbox"){
+    if (event.target.checked) {
+      selectedEmployeesCollection.push(event.target.dataset.id);
+    }
+    
+    if (event.target.checked === false) {
+			selectedEmployeesCollection.splice(selectedEmployeesCollection.indexOf(event.target.dataset.id), 1);
+		}
+  }
+});
 
-// fireBtn.addEventListener('change', (event) => {
-// 	employeesCollection.forEach((item, index) => {
-//   	  if (selectedEmploees.find(item.id)) {
-//       	employeesCollection.splice(index, 1);
-//       }
-//   })
+btnDismiss.addEventListener('click', (event) => {
+  const lostEmployees = employeesCollection.filter(item => {                // {id: 0, name: 'Вася'
   
-//   // тут сохранить в стор
-// }); 
+  	const result = String(selectedEmployeesCollection.find(element => {  	 // 2
+    	return String(item.id) === element;
+    }));
+
+    if (result === 'undefined') return true;
+    
+    return false;
+    
+  });
+  employeesCollection = [...lostEmployees];
+  saveData();
+  sumSalary = 0;
+  drawUI();   
+}); 
+
+btnSum.addEventListener('click', (event) => {
+//selectedEmployeesCollection = ["0", "2", "4"];
+sumSalary = employeesCollection.reduce((acc, current) => {  // current - {id: 0, name: 'Вася', salary: 1200,}
+ 
+  if (selectedEmployeesCollection.find(element => element === String(current.id) )) {
+    return acc + Number(current.salary);
+  } 
+  
+  return acc;
+}, 0);
+
+drawUI();
+});
+
+btnSortYearTop.addEventListener('click', (event) => {
+	employeesCollection.sort((a, b) => a.birthYear - b.birthYear);
+  drawUI();
+});
+
+ 
+btnSortYearBootom.addEventListener('click', (event) => {
+	employeesCollection.sort((a, b) => a.birthYear - b.birthYear).reverse();
+   drawUI();
+});
+
+btnSortDateTop.addEventListener('click', (event) => {
+  employeesCollection.sort((a, b) => new Date(a.hiringDate) - new Date(b.hiringDate));
+  drawUI();
+}); 
 
 
-
+btnSortDateBottom.addEventListener('click', (event) => {
+  console.log(event);
+	employeesCollection.sort((a, b) => new Date(a.hiringDate) - new Date(b.hiringDate)).reverse();
+  drawUI();
+});
